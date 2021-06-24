a3bbccf9931f4d550367f1231bc9fad1f09c115a
mitsuhiro0602 <0hornet0@gmail.com>
2021-06-24 15:11:16 +0900

    japanese change

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,14 +25,14 @@ const About = ({endpoint}) => {
         </title>
       </Head>
       <h6 className="my-3 text-base font-medium">
-        I am currently pursuring B.Tech Degree(Final Year) in Cimputer
-        Science Engineering from Academy of Technology. I have 3+ years of
-        experience Engineering from Academy of Technoloy. I have a Youtube Channel
-        Where I teach Full Stack Web Development
+        <p>ただオシャレなサイトを作るのではなく、お客様の思いの伝わるデザイン、</p>
+        <p>そしてコンバージョンに繋がるデザイン設計をして制作いたします。</p><br />
+        <p>オウンドメディアの構築からLステップやメルマガを使った売り上げに直結する導線、</p>
+        <p>お客様を惹きつける文章テクニックを使って売り上げアップに貢献します。</p>
       </h6>
       <div className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100"
         style={{ marginLeft: '-1.5rem', marginRight: '-1.5rem' }}>
-        <h4 className="my-3 text-xl font-bold">What I offer</h4>
+        <h4 className="my-3 text-xl font-bold">私ができること</h4>
         <motion.div 
           className="grid gap-6 lg:grid-cols-2" 
           variants={stagger} 
@@ -57,33 +57,8 @@ const About = ({endpoint}) => {
 
 export default About;
 
-// !called every time the page refreshed
 export const getServersideProps = async(
   context: GetServerSidePropsContext
 ) => {
-  // console.log(process.env.VERCEL_URL)
-  
-  // // calculation
-  // const res = await fetch(`${process.env.VERCEL_URL}/api/services`)
-  // const data = await res.json()
   return { props: { endpoint: process.env.VERCEL_URL }};
 };
-
-// !called only during the build of the project
-// export const getStaticProps = async(
-//   context: GetStaticPropsContext
-// ) => {
-  
-//   // calculation
-//   const res = await fetch('http://localhost:3000/api/services')
-//   const data = await res.json()
-
-//   console.log('SERVER', services)
-
-//   return {
-//     props: {
-//       services: data.services,
-//     },
-
-//   }
-// }
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -20,32 +20,37 @@ const resume = () => {
       </Head>
         
       {/* educatiuon & exp */}
-      <div className="grid gap-6 md:grid-cols-2">
+      <div className="grid gap-6 md:grid-cols-1">
         <motion.div variants={fadeInUp} initial="initial" animate="animate">
-          <h5 className="my-3 text-2xl font-bold">Education</h5>
+          <h5 className="my-3 text-2xl font-bold">経歴</h5>
           <div>
-            <h5 className="my-2 text-xl font-bold">Computer Sience Engineering</h5>
-            <p className="font-semibold">Academy of Technology(2017-2021)</p>
+            <h5 className="my-2 text-xl font-bold">WEB制作 & マーケッター</h5>
             <p className="my-3">
-              I am currently pursuing 8.tech in computer science Engineering from academy of technology
+              愛知県出身で大学から東京に上京しました。大学では電子情報を扱い、<br />
+              様々なハードウェアの研究開発を行なっていました。<br /><br />
+              大学の２年生からWEB制作を趣味で始め、<br />
+              商品をセールスするためにはコピーライティング、マーケティングを学び、<br />
+              マーケティグ会社へ就職しました。
+              現在はWEB制作をしながら本業はSlerに<br />
+              常駐し上流工程の設計をしています。
             </p>
           </div>
         </motion.div>
-        <motion.div variants={fadeInUp} initial="initial" animate="animate">
-          <h5 className="my-3 text-2xl font-bold">Experience</h5>
+        {/* <motion.div variants={fadeInUp} initial="initial" animate="animate">
+          <h5 className="my-3 text-2xl font-bold">得意なこと</h5>
           <div>
-            <h5 className="my-2 text-xl font-bold">Software Engineer Jr.</h5>
+            <h5 className="my-2 text-xl font-bold">データ分析、行動</h5>
             <p className="font-semibold">TCS(2021-on)</p>
             <p className="my-3">
               I don't know why I am dogin this job 
             </p>
           </div>
-        </motion.div>
+        </motion.div> */}
       </div>
       {/* Language & tool */}
       <div className="grid gap-6 md:grid-cols-2">
         <motion.div variants={fadeInUp} initial="initial" animate="animate">
-          <h5 className="my-3 text-2xl font-bold">Languages & Frameworks</h5>
+          <h5 className="my-3 text-2xl font-bold">スキル一覧</h5>
           <div className="my-2">
             {languages.map(Language=>(
               <Bar data={Language} key={Language.name} />
@@ -53,7 +58,7 @@ const resume = () => {
           </div>
         </motion.div>
         <motion.div variants={fadeInUp} initial="initial" animate="animate">
-          <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
+          <h5 className="my-3 text-2xl font-bold">使えるツール</h5>
           <div className="my-2">
             {tools.map(tool=>(
               <Bar data={tool} key={tool.name} />
